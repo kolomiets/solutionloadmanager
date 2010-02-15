01342962cbc7cd4baf26e77248937ffd3b997b73
kolomiets <kolomiets@8a8acfac-1a64-11df-bf36-6d084ca66768>
2010-02-15 19:40:10 +0000

    [tag] Tag for 0.1.1 version

---
+++ tags/0.1.1/Sources/SolutionLoadManager/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace EMCCaptiva.SolutionLoadManager
+{
+    static class GuidList
+    {
+        public const string guidSolutionLoadManagerPkgString = "2c8b59e2-48fb-4629-896f-4a550925e24c";
+        public const string guidSolutionLoadManagerCmdSetString = "fc8076ea-63e2-4fcf-a1d7-6aab2c322ad1";
+
+        public static readonly Guid guidSolutionLoadManagerCmdSet = new Guid(guidSolutionLoadManagerCmdSetString);
+    };
+}
