744f07fdcbbfc5297941f5ce82b6d4cf38e6201c
kolomiets <kolomiets@8a8acfac-1a64-11df-bf36-6d084ca66768>
2010-02-17 12:19:07 +0000

    [tag] Tag for 0.2 version

---
+++ tags/0.2/Sources/SolutionLoadManager/PkgCmdID.cs
@@ -0,0 +1,12 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace EMCCaptiva.SolutionLoadManager
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidSolutionLoadManager = 0x100;
+        public const uint cmdidSolutionLoadManagerContext = 0x101;
+    };
+}
---
+++ tags/0.2/Sources/SolutionLoadManager/ProjectInfo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Drawing;
+
+namespace EMCCaptiva.SolutionLoadManager
+{
+    [Serializable]
+    public enum LoadPriority
+    {
+        DemandLoad,
+        BackgroundLoad,
+        LoadIfNeeded,
+        ExplicitLoadOnly
+    }
+
+    [Serializable]
+    public class ProjectInfo
+    {
+        private readonly List<ProjectInfo> m_Children = new List<ProjectInfo>();
+
+        public ProjectInfo(String name, Guid projectId, LoadPriority priority, ProjectInfo parent)
+        {
+            Name = name;
+            ProjectId = projectId;
+            Priority = priority;
+            Parent = parent;
+        }
+
+        public Bitmap Icon { get; set; }
+
+        public String Name { get; private set; }
+
+        public Guid ProjectId { get; private set; }
+
+        public LoadPriority Priority { get; set; }
+
+        public ProjectInfo Parent { get; private set; }
+        
+        public List<ProjectInfo> Children 
+        {
+            get { return m_Children; }
+        }
+    }
+}
