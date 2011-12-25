390fb8e48e0ff677199ca12062fdde33c397e7cf
Kolomiets <kolomiets@gmail.com>
2011-12-25 07:49:53 +0000

    Simplify ProjectInfo implementation and explicitly map LoadPriority enumeration values to _VSProjectLoadPriority enumeration.

--- Sources/SolutionLoadManager/ProjectInfo.cs
+++ Sources/SolutionLoadManager/ProjectInfo.cs
@@ -3,29 +3,29 @@ using System.Collections.Generic;
 using System.Text;
 using System.Runtime.Serialization;
 using System.Drawing;
+using Microsoft.VisualStudio.Shell.Interop;
 
 namespace Kolos.SolutionLoadManager
 {
     [Serializable]
     public enum LoadPriority
     {
-        DemandLoad,
-        BackgroundLoad,
-        LoadIfNeeded,
-        ExplicitLoadOnly
+        DemandLoad = _VSProjectLoadPriority.PLP_DemandLoad,
+        BackgroundLoad = _VSProjectLoadPriority.PLP_BackgroundLoad,
+        LoadIfNeeded = _VSProjectLoadPriority.PLP_LoadIfNeeded,
+        ExplicitLoadOnly = _VSProjectLoadPriority.PLP_ExplicitLoadOnly
     }
 
     [Serializable]
     public class ProjectInfo
     {
-        private readonly List<ProjectInfo> m_Children = new List<ProjectInfo>();
-
         public ProjectInfo(String name, Guid projectId, LoadPriority priority, ProjectInfo parent)
         {
             Name = name;
             ProjectId = projectId;
             Priority = priority;
             Parent = parent;
+            Children = new List<ProjectInfo>();
         }
 
         public Bitmap Icon { get; set; }
@@ -38,9 +38,6 @@ namespace Kolos.SolutionLoadManager
 
         public ProjectInfo Parent { get; private set; }
         
-        public List<ProjectInfo> Children 
-        {
-            get { return m_Children; }
-        }
+        public List<ProjectInfo> Children { get; private set; }
     }
 }
