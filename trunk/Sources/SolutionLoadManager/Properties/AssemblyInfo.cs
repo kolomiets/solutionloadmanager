763f966d77a76bb04557566f37bf073164eb5467
kolomiets@gmail.com <kolomiets@gmail.com@8a8acfac-1a64-11df-bf36-6d084ca66768>
2011-12-30 19:05:30 +0000

    Update AssemblyInfo with actual information.

--- trunk/Sources/SolutionLoadManager/Properties/AssemblyInfo.cs
+++ trunk/Sources/SolutionLoadManager/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SolutionLoadManager")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Solution Load Manager VS Extension")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("EMC Captiva")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SolutionLoadManager")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
 
 
 
